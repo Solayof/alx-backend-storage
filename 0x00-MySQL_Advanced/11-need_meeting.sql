f64c40bba4eeefb5887f84400c5658e37dba7ce0
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-06-13 20:07:30 +0100

    task 11 Updated

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,11 +1,7 @@
 -- script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
 
-DELIMITER $$
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT
-BEGIN
-    RETURN (IF (b = 0, 0, a / b));
-END
-$$
-DELIMITER ;
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name FROM students 
+WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
