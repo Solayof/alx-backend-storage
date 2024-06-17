fe149fe9236e5e877ce867834809aab9f1dc2917
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-06-17 15:56:58 +0100

    task 12 created

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""script that provides some stats about Nginx logs stored in MongoDB:"""
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client.logs
+
+print(db.nginx.estimated_document_count(), "logs")
+print("Methods:")
+
+for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    print(f"\tmethod {method}: {db.nginx.count_documents({'method': method})}")
+
+count = db.nginx.count_documents(
+    {"method": "GET", "path": {"$regex": "^/.*status.*$"}}
+    )
+print(count, "status", "check")
