ec5479018c08fc42f79a24695229b06af5c9bd4a
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-06-18 01:50:33 +0100

    task 101 updated

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -7,6 +7,6 @@ def top_students(mongo_collection):
     return mongo_collection.aggregate([
         {
             "$project":
-            {"name": "$name", "averagescore": {"$avg": "$topics.score"}}
+            {"name": "$name", "averageScore": {"$avg": "$topics.score"}}
             }, {"$sort": {"averageScore": -1}}
             ])
