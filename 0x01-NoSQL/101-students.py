27d8f6a7470c939772f5783cdc45adecf5dc8d0e
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-06-18 01:46:39 +0100

    task 101 created

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """sort mongo collection"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+            {"name": "$name", "averagescore": {"$avg": "$topics.score"}}
+            }, {"$sort": {"averageScore": -1}}
+            ])
