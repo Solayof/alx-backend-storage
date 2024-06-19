f1c4b3c22c412d48194b35b2871381413eb7eacd
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-06-19 12:17:14 +0100

    task 1 created

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 """Redis Module"""
 import redis
+from functools import wraps
 from typing import Union, Callable, Optional
 from uuid import uuid4
 UnionOfTypes = Union[str, bytes, int, float]
+
+
+def count_calls(method: Callable) -> Callable:
+    """count numbers of time calche methods are called"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args,**kwargs)
+    return wrapper
 
 
 class Cache:
@@ -13,7 +26,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb
 
-
+    @count_calls
     def store(self, data: UnionOfTypes):
         """
         method that takes a data argument and returns a string. The method
@@ -22,3 +35,16 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
+        """retrieves data with the given key"""
+        data = self._redis.get(key)
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """get a string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """get an int"""
+        return self.get(key, int)
